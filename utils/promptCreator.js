8981eb987bdc6bf0f5c8bb58e89a36c7f4673ba5
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-01-14 17:08:55 +0100

    Written the prompt

--- utils/promptCreator.js
+++ utils/promptCreator.js
@@ -1,7 +1,21 @@
-export function createPrompt (amountOfQuestions, amountOfOptions, amountOfMultipleAnswerQuestions, ignoreContext, language, content) {
+// You will be presented with a transcript acquired through recording of a class and then passed through speech to text service. This transcript will not be properly formated and can contain mistakes. Your job is to look through it and remove any irregularities and make readable text out of the transcript. It shouldn't change much just places with problems. The ideas should remain the same.
+// Please output the ready text in the following format:
+// {
+//   "fixedText": ""
+// }
+
+function createPrompt (transcript) {
     let prompt = "";
 
-    console.log(prompt)
+    prompt += "You will be presented with a transcript acquired through recording of a class and then passed through speech to text service. This transcript will not be properly formated and can contain mistakes. Your job is to look through it and remove any irregularities and make readable text out of the transcript. It shouldn't change much just places with problems. The ideas should remain the same.";
+    prompt += "Please output the ready text in the following format:";
+    prompt += "{\n  \"fixedTranscript\": \"\"\n}";
+    prompt += "Here is the transcript that you need to work on: ";
+    prompt += transcript;
 
-    return prompt
+    console.log(prompt);
+
+    return prompt;
 }
+
+module.exports = {createPrompt};
